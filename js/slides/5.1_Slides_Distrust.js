7f02a52f021109c41abcce523d194f2c6bbd62d8
Arefeh Ahmadi <arefehahmadi902@gmail.com>
2022-08-26 18:53:43 +0430

    trying to split slide 5

--- js/slides/5.1_Slides_Distrust.js
+++ js/slides/5.1_Slides_Distrust.js
@@ -123,30 +123,6 @@ SLIDES.push({
 		_hide(o.roundsLabel); _fadeIn(o.roundsLabel, 500);
 		_hide(o.roundsSlider); _fadeIn(o.roundsSlider, 500);
 
-		// Continue...
-		// listen(_.misc, "tournament/autoplay/start",function(){
-		// 	if(_showContinue) _showContinue();
-		// });
-		// var _showContinue = function(){
-		// 	_showContinue = null;
-		// 	var x = 125;
-		// 	var y = 430;
-		// 	self.add({
-		// 		id:"continueLabel", type:"TextBox",
-		// 		x:x, y:y+5, width:200, height:50,
-		// 		align:"right", color:"#aaa", size:17,
-		// 		text_id:"distrust_2_end"
-		// 	});
-		// 	self.add({
-		// 		id:"continueButton", type:"Button",
-		// 		x:x+215, y:y, size:"short",
-		// 		text_id:"label_continue",
-		// 		message: "slideshow/next"
-		// 	});
-		// 	_hide(o.continueLabel); _fadeIn(o.continueLabel, 100);
-		// 	_hide(o.continueButton); _fadeIn(o.continueButton, 100);
-		// };
-
 	},
 	onend: function(self){
 		unlisten(_.misc);
@@ -157,3 +133,147 @@ SLIDES.push({
 		if(o.continueButton) self.remove("continueButton");
 	}
 });
+
+// // Explain that...
+// SLIDES.push({
+// 	onstart: function(self){
+
+// 		var o = self.objects;
+
+// 		// Words
+// 		o.text.setTextID("distrust_3");
+// 		_hide(o.text); _fadeIn(o.text, 100);
+
+// 		// Worse...
+// 		self.add({
+// 			id:"next", type:"Button",
+// 			x:0, y:450, size:"long",
+// 			text_id:"distrust_3_btn",
+// 			message: "slideshow/next"
+// 		});
+// 		_hide(o.next); _fadeIn(o.next, 400);
+
+// 	},
+// 	onend: function(self){	
+// 		self.remove("text");
+// 		self.remove("next");
+// 	}
+// });
+
+// // Change PAYOFFS
+// SLIDES.push({
+// 	onstart: function(self){
+
+// 		var o = self.objects;
+// 		_.misc = {};
+
+// 		// Words
+// 		self.add({
+// 			id:"text", type:"TextBox",
+// 			x:0, y:0, width:450, height:500,
+// 			text_id:"distrust_4"
+// 		});
+// 		_hide(o.text); _fadeIn(o.text, 100);
+
+// 		// PAYOFFS
+// 		self.add({
+// 			id:"payoffs", type:"PayoffsUI",
+// 			x:105, y:73, scale:0.8, slideshow:self
+// 		});
+// 		_hide(o.payoffs); _fadeIn(o.payoffs, 300);
+// 		listen(_.misc, "payoffs/onchange", function(value){
+// 			o.tournament.reset();
+// 		});
+
+// 		// More words
+// 		self.add({
+// 			id:"text2", type:"TextBox",
+// 			x:0, y:320, width:450, height:500,
+// 			text_id:"distrust_4_2"
+// 		});
+// 		_hide(o.text2); _fadeIn(o.text2, 500);
+
+// 		// Note 1
+// 		var _showContinue = function(){
+// 			_showContinue = null;
+// 			self.add({
+// 				id:"continueLabel", type:"TextBox",
+// 				x:0, y:480, width:320, 
+// 				align:"right", color:"#aaa", size:17,
+// 				text_id:"distrust_4_note"
+// 			});
+// 			self.add({
+// 				id:"continueButton", type:"Button",
+// 				x:340, y:471, size:"short",
+// 				text_id:"label_continue",
+// 				message: "slideshow/next"
+// 			});
+// 			_hide(o.continueLabel); _fadeIn(o.continueLabel, 100);
+// 			_hide(o.continueButton); _fadeIn(o.continueButton, 100);
+// 		};
+// 		listen(_.misc, "tournament/autoplay/start",function(){
+// 			if(_showContinue) _showContinue();
+// 		});
+
+// 		// Note 2
+// 		self.add({
+// 			id:"note2", type:"TextBox",
+// 			x:583, y:510, width:300, height:50,
+// 			align:"center", color:"#aaa", size:17,
+// 			text_id:"distrust_4_note_2"
+// 		});
+// 		_hide(o.note2); _fadeIn(o.note2, 500);
+
+// 		// TOURNAMENT
+// 		Tournament.resetGlobalVariables();
+// 		Tournament.INITIAL_AGENTS = [
+// 			{strategy:"all_c", count:23},
+// 			{strategy:"all_d", count:1},
+// 			{strategy:"tft", count:1}
+// 		];
+// 		Tournament.NUM_TURNS = 10;
+// 		o.tournament.reset();
+
+// 	},
+// 	onend: function(self){
+// 		unlisten(_.misc);
+// 		self.remove("text");
+// 		self.remove("payoffs");
+// 		self.remove("text2");
+// 		self.remove("note2");
+// 		var o = self.objects;
+// 		if(o.continueLabel) self.remove("continueLabel");
+// 		if(o.continueButton) self.remove("continueButton");
+// 	}
+// });
+
+// // Explain that...
+// SLIDES.push({
+// 	onstart: function(self){
+
+// 		var o = self.objects;
+
+// 		// Words
+// 		self.add({
+// 			id:"text", type:"TextBox",
+// 			x:0, y:30, width:450, height:500,
+// 			text_id:"distrust_5"
+// 		});
+// 		_hide(o.text); _fadeIn(o.text, 100);
+
+// 		// Worse...
+// 		self.add({
+// 			id:"next", type:"Button",
+// 			x:0, y:450, size:"long",
+// 			text_id:"distrust_5_btn",
+// 			message: "slideshow/scratch"
+// 		});
+// 		_hide(o.next); _fadeIn(o.next, 400);
+
+// 	},
+// 	onend: function(self){	
+// 		unlisten(_);
+// 		unlisten(_.misc);
+// 		self.clear();
+// 	}
+// });
